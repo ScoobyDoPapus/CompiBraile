46614587335f2b8a053e06f983bb8356ca417196
nachomarin97 <nachomarin97@gmail.com>
2018-05-21 23:41:14 -0600

    Add files via upload

---
+++ main/main.java
@@ -0,0 +1,13 @@
+package main;
+
+import gui.MainWindow;
+
+public class main {
+
+    public static void main(String[] args) {
+        
+        MainWindow mw = new MainWindow();
+        
+    }
+    
+}
